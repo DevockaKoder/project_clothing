21342cb5f40e4f6fb3114c1e38307a2d9c63f4b3
DevockaKoder <56919331+DevockaKoder@users.noreply.github.com>
2022-11-03 00:36:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,16 @@ import numpy as np
 from PIL import Image
 
 #создаем функцию загрузки файла
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+
 def load_image():
-    uploaded_file = st.file_uploader(label='Âûáåðèòå èçîáðàæåíèå äëÿ ðàñïîçíàâàíèÿ')
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -15,19 +23,17 @@ def load_image():
     else:
         return None
 
-def preprocess_image(img):
-    img = image.load_img(img_path, target_size=(28, 28), color_mode = "grayscale")
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(str(cl[1]).replace('_'," "), cl[2])
 
-
-st.title('Распознавание одежды на фото)
+st.title('Распознавание изображений')
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
     x = preprocess_image(img)
     preds = model.predict(x)
     st.write('**Результаты распознавания:**')
+    print_predictions(preds)авания:**')
     print_predictions(preds)
